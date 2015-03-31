09fde65e4ca56291b761730f860076899dc0893e
Blake Tarter <blake@codekoalas.com>
2015-03-31 16:50:58 -0500

    should probably update my tests

--- test/test-app.js
+++ test/test-app.js
@@ -18,7 +18,6 @@ describe('one-pager:app', function () {
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
       'package.json',
       '.editorconfig',
       '.jshintrc',
